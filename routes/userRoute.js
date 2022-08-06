30a76ad16e821592884efb7e96fe5af100740ed9
UzairDamonse <uzairdamonse03@gmail.com>
2022-08-06 10:10:51 +0200

    first commit

---
+++ routes/userRoute.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const con = require("../lib/dbConnection");
+
+router.get("/", (req, res) => {
+  try {
+    con.query("SELECT * FROM users", (err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
